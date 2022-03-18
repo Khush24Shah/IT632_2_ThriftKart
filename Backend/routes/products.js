f0caaa0d2f1dd66e3399d74dc003917f83a4608d
Ekansh Gupta <ekanshgupta98@gmail.com>
2022-03-18 18:09:54 +0530

    ...

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {
-  getAllProducts,
-  getAllProductsStatic,
-} = require('../controllers/products')
+const { getAllProducts, getAllProductsStatic } = require("../controllers/products");
 
-router.route('/').get(getAllProducts)
-router.route('/static').get(getAllProductsStatic)
+router.route("/").get(getAllProducts);
 
-module.exports = router
+module.exports = router;
