338adb9c66609ee81b1d133880b2f838fd98f587
Shailesh Jadhav <202112051@daiict.ac.in>
2022-03-19 15:03:15 +0530

    BCKNG customer schema added

---
+++ Backend/models/customer.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const customerSchema = new mongoose.Schema({
+  id: {
+    type: integer,
+    unique: true,
+    required: [true, 'id must be provided'],
+  },
+  fname: {
+      type: String,
+      required: [true, "first name cant be null"]
+  },
+  lname: {
+    type: String,
+    required: [true, "last name cant be null"]
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+        validator: function(v) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email"
+    },
+    required: [true, "Email required"]
+    },
+
+    mobile: {
+        type: String,
+        trim: true,
+        unique: true,
+        validate: {
+          validator: function (v) {
+            return /^[0-9]{10}/.test(v);
+          },
+          message: '{VALUE} is not a valid 10 digit number!'
+        },
+        required: [true,"mobile number is required"],
+    },
+    adress: {
+        type: String,
+        required: [true,"adress  is required"],
+
+    },
+    dob :{
+        type: Date,
+    required: true,
+    trim: true,
+    }
+
+  
+})
+
+module.exports = mongoose.model('Customer', customerSchema)
