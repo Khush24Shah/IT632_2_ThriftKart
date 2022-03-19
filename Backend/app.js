04ef003e9ba013b4ad4af790d43a87d59ecbf866
Ekansh Gupta <ekanshgupta98@gmail.com>
2022-03-19 15:16:11 +0530

    BACKEND: auth ongoing

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,30 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDB = require('./db/connect');
-const productsRouter = require('./routes/products');
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.json());
-
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 // routes
 
-app.get('/', (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+app.get("/", (req, res) => {
+	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
-app.use('/api/v1/products', productsRouter);
+app.use("/api/v1/products", productsRouter);
 
 // products route
 
@@ -29,13 +34,13 @@ app.use(errorMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    // connectDB
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => console.log(`Server is listening port ${port}...`));
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		// connectDB
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => console.log(`Server is listening port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
---
+++ Backend/middleware/authVerify.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+User = require("../models/user");
+
+const verifyToken = (req, res, next) => {
+	if (req.headers && req.headers.authorization && req.headers.authorization.split(" ")[0] === "JWT") {
+		jwt.verify(req.headers.authorization.split(" ")[1], process.env.API_SECRET, function (err, decode) {
+			if (err) req.user = undefined;
+			User.findOne({
+				_id: decode.id,
+			}).exec((err, user) => {
+				if (err) {
+					res.status(500).send({
+						message: err,
+					});
+				} else {
+					req.user = user;
+					next();
+				}
+			});
+		});
+	} else {
+		req.user = undefined;
+		next();
+	}
+};
+module.exports = verifyToken;
