7183f94115e84c1136c88314d16241b9fffbea00
Shailesh Jadhav <202112051@daiict.ac.in>
2022-03-28 00:01:40 +0530

    BCKND otp authentication done with session
    done otp verification , but changed request to GET

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -6,13 +6,12 @@ var nodemailer = require('nodemailer');
 
 exports.emailsend = (req, res)=>{
     
-    var otp = Math.floor(100000 + Math.random() * 900000);
-	console.log(otp);
+    var otp1 = Math.floor(100000 + Math.random() * 900000);
+	console.log(otp1);
 	
+	req.session.otp=bcrypt.hashSync(otp1.toString(10), 8);
 
-	req.session.otp=bcrypt.hashSync(otp.toString(10), 8);
-
-	console.log(otp.toString(10));
+	
 	var transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
@@ -25,7 +24,7 @@ exports.emailsend = (req, res)=>{
 		from: 'thriftkart11@gmail.com', // sender address
 		to: req.query.email, // list of receivers
 		subject: 'Thrift Kart Signup OTP', // Subject line
-		html: '<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2"><div style="margin:50px auto;width:70%;padding:20px 0"><div style="border-bottom:1px solid #eee"><a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">ThriftKart</a></div><p style="font-size:1.1em">Hi,</p><p>Thank you for choosing ThriftKart. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'+otp+'</h2><p style="font-size:0.9em;">Regards,<br />Project Team</p><hr style="border:none;border-top:1px solid #eee" /><div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300"><p>ThriftKart Inc</p><p>Grp -2 , SE project</p><p>Sem 2 Msc IT, Daiict</p></div></div></div>'// plain text body
+		html: '<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2"><div style="margin:50px auto;width:70%;padding:20px 0"><div style="border-bottom:1px solid #eee"><a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">ThriftKart</a></div><p style="font-size:1.1em">Hi,</p><p>Thank you for choosing ThriftKart. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'+otp1+'</h2><p style="font-size:0.9em;">Regards,<br />Project Team</p><hr style="border:none;border-top:1px solid #eee" /><div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300"><p>ThriftKart Inc</p><p>Grp -2 , SE project</p><p>Sem 2 Msc IT, Daiict</p></div></div></div>'// plain text body
 	  };
 	  transporter.sendMail(mailOptions, function (err, info) {
 		if(err)
