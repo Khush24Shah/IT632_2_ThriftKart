39f40a3d0710ef0fb0deb39f01d12486b39c0613
Shailesh Jadhav <202112051@daiict.ac.in>
2022-04-20 15:00:09 +0530

    BCKND cart changes & Analysis class diagram

--- Backend/routes/cart.js
+++ Backend/routes/cart.js
@@ -61,14 +61,62 @@ router.post("/", verifyToken, async (req, res) => {
 //UPDATE
 router.put("/:id", verifyToken, async (req, res) => {
   try {
-    const updatedCart = await Cart.findByIdAndUpdate(
+
+    const newCart = new Cart(req.body);
+  const userId = req.user._id; 
+   let cart = await Cart.findOne({userId:req.user._id});
+        console.log(req.body.products.productId);
+        let product = await Product.findOne({_id: req.body.products.productId});
+        if(!product){
+            res.status(404).send('Item not found!')
+        }
+        const price = product.price;
+        const name = product.name;
+        const productId = req.body.products.productId;
+        let qty =  req.body.products.qty;
+        
+        if(!cart)
+        {
+          return res.status(400).send("Cart not found");
+        }
+        else
+        {
+          console.log("here in else");
+          let itemIndex = cart.products.findIndex(p => p.productId == productId);
+          
+          if(itemIndex == -1)
+            {
+              return res.status(400).send("Product not found");
+            }
+            else
+            {
+              console.log("product item");
+         
+              let productItem = cart.products[itemIndex];
+              productItem.qty = qty;
+              cart.products[itemIndex] = productItem;
+              console.log(cart);
+            }
+            cart.bill = cart.products.reduce((sum, product) => sum + product.price * product.qty,0);
+            if(product.qty == 0)
+            {
+             cart.products.splice(itemIndex,1);
+            }   
+            }
+            cart = await cart.save();
+           // console.log(cart);
+            return res.status(201).send(cart);
+            
+        
+
+    /* const updatedCart = await Cart.findByIdAndUpdate(
       req.params.id,
       {
         $set: req.body,
       },
       { new: true }
     );
-    res.status(200).json(updatedCart);
+    res.status(200).json(updatedCart); */
   } catch (err) {
     res.status(500).json(err);
   }
