ef25312e0d1a48ff47efcbde0bd094cde4a54b46
Shailesh Jadhav <202112051@daiict.ac.in>
2022-05-10 16:34:05 +0530

    order route

--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -1,10 +1,14 @@
 const Order = require("../models/Order");
 const Product = require("../models/product");
+const orderController = require('../controllers/order');
 const verifyToken = require("../middleware/authVerify");
 
 
 const router = require("express").Router();
 
+router.get('/order/:id',orderController.get_orders);
 
+router.get('/order/',orderController.get_all_orders);
+router.post('/order/:id',orderController.checkout);
 
 module.exports = router;
